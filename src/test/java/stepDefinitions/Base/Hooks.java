d78854c05c5fa60b84bc915ac957ed4fd936a337
Claudio.Correia <claudiooartista@hotmail.co.uk>
2025-09-22 17:36:09 +0100

    Added the screenshot method for failed scenarios

--- src/test/java/stepDefinitions/Base/Hooks.java
+++ src/test/java/stepDefinitions/Base/Hooks.java
@@ -1,7 +1,13 @@
 package stepDefinitions.Base;
 
 import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import io.cucumber.messages.types.Timestamp;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
 import static driver.DriverFactory.cleanUpDriver;
 import static driver.DriverFactory.getDriver;
 
@@ -10,6 +16,16 @@ public class Hooks {
     @Before
     public void setUp() {
         getDriver();
+    }
+
+    @AfterStep
+    public void captureScreenshot(Scenario scenario) {
+        if(scenario.isFailed()) {
+            String timestamp = String.valueOf(System.currentTimeMillis());
+            byte[] screenshot = ((TakesScreenshot) getDriver())
+                    .getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot, "image/png", "screenshot_" + timestamp + ".png");
+        }
     }
 
     @After
